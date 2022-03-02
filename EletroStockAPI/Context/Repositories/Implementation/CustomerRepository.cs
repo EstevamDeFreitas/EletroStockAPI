b6601a9d925fe954c9ad10033639e58ca95236cc
Estevam <estevamdefreitas37@gmail.com>
2022-03-02 20:17:58 -0300

    Criado o repositório do cliente

---
+++ EletroStockAPI/Context/Repositories/Implementation/CustomerRepository.cs
@@ -0,0 +1,64 @@
+﻿using EletroStockAPI.Context.Models;
+using EletroStockAPI.Context.Repositories.Interfaces;
+
+namespace EletroStockAPI.Context.Repositories.Implementation
+{
+    public class CustomerRepository : RepositoryBase, ICustomerRepository
+    {
+        public CustomerRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public bool CreateCustomer(Customer customer)
+        {
+            try
+            {
+                context.Add(customer);
+                EndTransaction(true);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCustomer(Customer customer)
+        {
+            try
+            {
+                context.Remove(customer);
+                EndTransaction(true);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public Customer? GetCustomer(string customerId)
+        {
+            return context.Customers.Find(customerId);
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return context.Customers.ToList();
+        }
+
+        public bool UpdateCustomer(Customer customer)
+        {
+            try
+            {
+                context.Update(customer);
+                EndTransaction(true);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return true;
+            }
+        }
+    }
+}
--- EletroStockAPI/Context/Repositories/Interfaces/ICustomerRepository.cs
+++ EletroStockAPI/Context/Repositories/Interfaces/ICustomerRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace EletroStockAPI.Context.Repositories.Interfaces
+﻿using EletroStockAPI.Context.Models;
+
+namespace EletroStockAPI.Context.Repositories.Interfaces
 {
     public interface ICustomerRepository : IRepositoryBase
     {
-
+        public Customer? GetCustomer(string customerId);
+        public List<Customer> GetCustomers();
+        public bool DeleteCustomer(Customer customer);
+        public bool UpdateCustomer(Customer customer);
+        public bool CreateCustomer(Customer customer);
     }
 }
