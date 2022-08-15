02e7d09d1b4e3276efb89c9bad5c9ca74cf9f942
Estevam de Freitas Junior <estevamdefreitas37@gmail.com>
2022-08-14 21:18:24 -0300

    Adicionado entidade base e entidade do cliente

---
+++ Domain/Entities/Customer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    [Table("etk_customers")]
+    public class Customer : EntityBase
+    {
+        [Required]
+        [Column("etk_email")]
+        [MaxLength(50)]
+        public string Email { get; set; }
+        [Required]
+        [Column("etk_password")]
+        [MaxLength(255), MinLength(8)]
+        public string Password { get; set; }
+        [Required]
+        [Column("etk_name")]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [Column("etk_gender")]
+        [MaxLength(1)]
+        public char Gender { get; set; }
+        [Required]
+        [Column("etk_gender")]
+        public DateTime BirthDate { get; set; }
+        [Required]
+        [Column("etk_cpf")]
+        [StringLength(11)]
+        public string CPF { get; set; }
+        [Required]
+        [Column("etk_phone_type")]
+        [MaxLength(30)]
+        public string PhoneType { get; set; }
+        [Required]
+        [Column("etk_phone_code")]
+        [MaxLength(3)]
+        public decimal PhoneCode { get; set; }
+        [Required]
+        [Column("etk_phone_number")]
+        [MaxLength(9)]
+        public decimal PhoneNumber { get; set; }
+        [Required]
+        [Column("etk_ranking")]
+        public int Ranking { get; set; }
+    }
+}
---
+++ Domain/Entities/EntityBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class EntityBase
+    {
+        [Key]
+        [Column("etk_id")]
+        public Guid Id { get; set; }
+        [Required]
+        [Column("etk_dt_creation")]
+        public DateTime DateCreation { get; set; }
+        [Required]
+        [Column("etk_dt_modified")]
+        public DateTime DateModification { get; set; }
+
+        public void Generate()
+        {
+            Id = Guid.NewGuid();
+            DateCreation = DateTime.Now;
+            DateModification = DateTime.Now;
+        }
+    }
+}
