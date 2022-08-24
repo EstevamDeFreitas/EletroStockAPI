c73950642b77fa71896dac6b60b7ffa2ade8442c
Estevam de Freitas Junior <estevamdefreitas37@gmail.com>
2022-08-24 07:23:21 -0300

    Adicionado entidades

---
+++ Domain/Entities/CardFlag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    [Table("etk_card_flags")]
+    public class CardFlag : EntityBase
+    {
+        [Required]
+        [Column("name")]
+        [StringLength(75)]
+        public string Name { get; set; }
+    }
+}
---
+++ Domain/Entities/CreditCard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    [Table("etk_credit_cards")]
+    public class CreditCard : EntityBase
+    {
+        [Required]
+        [Column("card_number")]
+        [StringLength(16)]
+        public string CardNumber { get; set; }
+        [Required]
+        [Column("owner_name")]
+        [MaxLength(255)]
+        public string OwnerName { get; set; }
+        [Required]
+        [Column("name")]
+        [StringLength(3)]
+        public string SecurityCode { get; set; }
+        [Required]
+        public Guid CustomerId { get; set; }
+        [Required]
+        public Guid CardFlagId { get; set; }
+
+        public CardFlag CardFlag { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
