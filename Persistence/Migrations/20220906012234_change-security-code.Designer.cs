3d171b3b787fb181e71c5e1d7d56515fdec57572
Estevam De Freitas <estevamdefreitas37@gmail.com>
2022-09-05 22:23:12 -0300

    Alterado nome do campo de security code

--- Persistence/Migrations/20220906012234_change-security-code.Designer.cs
+++ Persistence/Migrations/20220906012234_change-security-code.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Persistence.Database;
@@ -11,9 +12,10 @@ using Persistence.Database;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(EletroStockContext))]
-    partial class EletroStockContextModelSnapshot : ModelSnapshot
+    [Migration("20220906012234_change-security-code")]
+    partial class changesecuritycode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Persistence/Migrations/20220906012234_change-security-code.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class changesecuritycode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "etk_credit_cards",
+                newName: "security_code");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "etk_card_flags",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(75)",
+                oldMaxLength: 75);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "security_code",
+                table: "etk_credit_cards",
+                newName: "name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "etk_card_flags",
+                type: "character varying(75)",
+                maxLength: 75,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32);
+        }
+    }
+}
