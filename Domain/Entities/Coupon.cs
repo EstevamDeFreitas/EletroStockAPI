b0a9ba0e9da6b6d3b47ec2fdcced5e255e9737af
Estevam de Freitas Junior <estevamdefreitas37@gmail.com>
2022-09-23 08:37:13 -0300

    Criado entidade para cupons

---
+++ Domain/Entities/Coupon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    [Table("etk_coupons")]
+    public class Coupon : EntityBase
+    {
+        [Required]
+        [Column("name")]
+        public string Name { get; set; }
+        [Required]
+        [Column("coupon_type")]
+        public CouponType CouponType { get; set; }
+        [Required]
+        [Column("dt_validity")]
+        public DateTime Validity { get; set; }
+        [Required]
+        [Column("has_validity")]
+        public bool HasValidity { get; set; }
+        [Required]
+        [Column("value")]
+        public decimal Value { get; set; }
+    }
+
+    public enum CouponType
+    {
+        Percentage,
+        Value
+    }
+}
---
+++ Domain/Entities/CouponCustomer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    [Table("etk_customer_coupons")]
+    public class CouponCustomer
+    {
+        [Required]
+        [Column("customer_id")]
+        public Guid CustomerId { get; set; }
+        [Required]
+        [Column("coupon_id")]
+        public Guid CouponId { get; set; }
+        [Required]
+        [Column("value_remaining")]
+        public decimal ValueRemaining { get; set; }
+
+        public Customer Customer { get; set; }
+        public Coupon Coupon { get; set; }
+    }
+}
